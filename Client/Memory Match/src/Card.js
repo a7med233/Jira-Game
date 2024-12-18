b43cfd3e4e37f59d2ff8af0c0a4baa922a53252f
Ahmed Emad Mohammed Abdelaziz Samra <ahmedemad23@mailbox.unideb.hu>
2024-12-18 13:00:18 +0100

    SMS-12 Creating the cards logic and basic style

---
+++ Client/Memory Match/src/Card.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Card.css';
+
+export default function Card({ card, handleChoice, flipped, disabled }) {
+  const handleClick = () => {
+    if (!disabled) {
+      handleChoice(card);
+    }
+  };
+
+  return (
+    <div className="card">
+      <div className={flipped ? 'flipped' : ''}>
+        <img src={card.src} alt="card front" className="front" />
+        <img
+          src="/img/cover.png"
+          alt="card back"
+          className="back"
+          onClick={handleClick}
+        />
+      </div>
+    </div>
+  );
+}
